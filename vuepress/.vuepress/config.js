782f20e976f620536b2d3f65ee5f247b3d803662
huhao <18255606178@163.com>
2021-08-27 10:34:04 +0800

    md

--- vuepress/.vuepress/config.js
+++ vuepress/.vuepress/config.js
@@ -1,11 +1,113 @@
-module.exports = {
-  title: "VuePress 云开发网站应用示例",
-  description: "基于云开发的 VuePress 网站应用示例",
-  base: "/vuepress/",
-  themeConfig: {
-    nav: [
-      { text: "首页", link: "/" },
-      { text: "指南", link: "/guides/" },
-    ],
+const { fs, path } = require('@vuepress/shared-utils')
+
+module.exports = ctx => ({
+	title: "我的收藏",
+	description: "基于云开发的 VuePress 网站应用示例",
+	base: "/vuepress/",
+  head:[
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style',  }]
+  ],
+  markdown: {
+    lineNumbers: true // 代码块显示行号
   },
-};
+	themeConfig: {
+		lastUpdated: 'Last Updated',
+		nav: [{
+				text: "首页",
+				link: "/"
+			},
+			{
+				text: "指南",
+				link: "/guides/"
+			},
+		],
+		editLinks: false,
+		editLinkText: '帮助我们改善此页面！',
+		locales: {
+			'/': {
+				editLinkText: '在 GitHub 上编辑此页',
+				nav: require('./nav/zh'),
+				sidebar: {
+					'/guides/': renderSiderBar()
+				}
+			}
+		}
+	},
+	plugins: [
+		['@vuepress/back-to-top', true],
+		['@vuepress/pwa', {
+			serviceWorker: true,
+			updatePopup: true
+		}],
+		['@vuepress/medium-zoom', true],
+		// ['@vuepress/google-analytics', {
+		//   ga: 'UA-145821923-1'
+		// }],
+		// ['vuepress-plugin-baidu-google-analytics', {
+		//   hm: '009a2f9b8cfc23cb5722f109462e450f',
+		//   ignore_hash: false
+		// }],
+		['container', {
+			type: 'vue',
+			before: '<pre class="vue-container"><code>',
+			after: '</code></pre>',
+		}],
+		['container', {
+			type: 'upgrade',
+			before: info => `<UpgradePath title="${info}">`,
+			after: '</UpgradePath>',
+		}],
+	],
+  extraWatchFiles: [
+    '.vuepress/nav/zh.js',
+  ]
+});
+
+function renderSiderBar() {
+	return (
+		[
+			{
+				title: 'HTML、CSS、JavaScript',
+				collapsable: false,
+				children: [
+					'front-end/JavaScript/browser',
+					'front-end/JavaScript/HTML',
+					'front-end/JavaScript/CSS',
+					'front-end/JavaScript/JavaScript',
+					'front-end/JavaScript/Ajax',
+					'front-end/JavaScript/layout',
+					'front-end/JavaScript/BFC',
+					'front-end/JavaScript/HTTP',
+					'front-end/JavaScript/SEO',
+					'front-end/JavaScript/cross-domain',
+					'front-end/JavaScript/data-structure',
+					'front-end/JavaScript/DOM',
+					'front-end/JavaScript/localStorage',
+				]
+			},
+			{
+				title: 'VUW',
+				collapsable: false,
+				children: [
+					'front-end/VUE/MVVM',
+				]
+			},
+			{
+				title: 'Node',
+				collapsable: false,
+				children: [
+					'front-end/Node/dataBase',
+				]
+			},
+		]
+	)
+}
--- vuepress/guides/README.md
+++ vuepress/guides/README.md
@@ -1,3 +1,65 @@
 # 指南
 
-这个页面展示项目的开发指南列表
+<a href="https://github.com/TencentCloudBase/cloudbase-templates"><img src="https://main.qcloudimg.com/raw/230c115bee4300384fa557710daa2928.jpg"></a>
+
+# 云开发 VuePress 网站应用示例
+
+这个目录是基于云开发的一个 [VuePress](https://vuepress.vuejs.org/) 网站应用示例，包含 VuePress 静态网站部署，可以基于 **[CloudBase Framework](https://github.com/TencentCloudBase/cloudbase-framework)** 框架将项目一键部署到云开发环境
+
+## 线上演示地址
+
+[https://framework-1258016615.tcloudbaseapp.com/vuepress/](https://framework-1258016615.tcloudbaseapp.com/vuepress/)
+
+点击下方按钮使用 [CloudBase Framework](https://github.com/TencentCloudBase/cloudbase-framework) 可以在云端一键部署本项目到自己的云开发账号上。
+
+[![](https://main.qcloudimg.com/raw/95b6b680ef97026ae10809dbd6516117.svg)](https://console.cloud.tencent.com/tcb/env/index?action=CreateAndDeployCloudBaseProject&appUrl=https%3A%2F%2Fgitee.com%2Fwarmthlinn_admin%2Fvuepress&branch=master)
+
+## 部署一个 VuePress 网站
+
+### 步骤一. 准备工作
+
+具体步骤请参照 [准备云开发环境和 CloudBase CLI 命令工具](https://github.com/TencentCloudBase/cloudbase-framework/blob/master/CLI_GUIDE.md)
+
+### 步骤二. 初始化应用示例
+
+在命令行执行
+
+```
+cloudbase init --template vuepress
+```
+
+### 步骤三. 一键部署
+
+进入到项目目录，在命令行执行
+
+```
+cloudbase framework deploy
+```
+
+## 开发命令及配置
+
+### 本地开发
+
+```
+npm run dev
+```
+
+### 上线部署
+
+```
+npm run deploy
+```
+
+### build
+
+```
+npm run build
+```
+
+### CloudBase Framework 相关开发配置
+
+查看 [CloudBase Framework 配置](https://github.com/TencentCloudBase/cloudbase-framework).
+
+### VuePress 相关开发配置
+
+查看 [VuePress 配置](https://vuepress.vuejs.org/zh/config/).
