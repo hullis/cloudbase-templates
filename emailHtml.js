eb74e24ea09d0d846e65f0b3160fad0dc20bd1b1
huhao <18255606178@163.com>
2021-10-15 10:22:47 +0800

    邮件

---
+++ emailHtml.js
@@ -0,0 +1,25 @@
+function fn() {
+
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </head>
+    <body>
+      <div>
+        <!-- 天气 -->
+        <div>
+          <p>
+            <b>push提醒:</b>
+            <span>cloudbase-templates项目正在部署云开发</span>
+          </p>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
+}
+
+module.exports = fn;
---
+++ index.js
@@ -0,0 +1,35 @@
+
+const sendEmail = require('./sendEmail');
+const emailHtml = require('./emailHtml');
+
+const {
+  fromDisplayText,
+  fromDisplaySubText,
+  user,
+  to,
+} = require('./config');
+
+async function init() {
+  try {
+    // 用邮件模版生成字符串
+    const htmlStr = emailHtml();
+
+    // 发送邮件;
+    sendEmail({
+      from: fromDisplayText,
+      to,
+      subject: fromDisplaySubText,
+      html: htmlStr,
+    });
+  } catch (e) {
+    // 发送邮件给自己提示
+    sendEmail({
+      from: '报错啦',
+      to: user,
+      subject: 'push失败',
+      html: '请查看github actions',
+    });
+  }
+}
+
+init();
