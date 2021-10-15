ca5c516dafae50395c5db41ccdbb470a1204b027
huhao <18255606178@163.com>
2021-10-15 09:16:22 +0800

    文件路径

--- docs/build.js
+++ docs/build.js
@@ -3,14 +3,14 @@ const { resolve } = require('path');
 const { existsSync, lstatSync, readdirSync } = require('fs');
 const { fs } = require('@vuepress/shared-utils')
 function readFileList(dir,fileList = []) {
-  const dirPath = resolve(`./guides/${dir}`);
-  const isDir = existsSync(dirPath) 
+  const dirPath = resolve(`./docs/guides/${dir}`);
+  const isDir = existsSync(dirPath)
   if (!isDir) {
     return fileList;
   }
   const files = readdirSync(dirPath);
   files.forEach((item) => {
-		var path = resolve(`./guides/${dir}/${item}`);
+		var path = resolve(`./docs/guides/${dir}/${item}`);
 		var state = fs.lstatSync(path);
 		if(state.isDirectory()){
 			// 文件夹
